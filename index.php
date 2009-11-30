52fa496f43a5d8f29ed9f3fafd48c756dbb09c63
Hugo Romano <hugoromano@gmail.com>
2009-11-30 14:14:34 +0000

    parse .txt .htm .csv .xls .doc in import folder

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 require_once "lib.php";
-// paste your emails in folder import/import.txt 
-$file = fopen("import/import.txt", "r");
+// files to import email, add them to folder import
 
-while (!feof($file)) {
-$line_of_text = fgets($file);
-$input .= $line_of_text;
+if ($handle = opendir('import/')) {
+echo "Processed:\n";
+	while (false !== ($file = readdir($handle))) {
+		if (preg_match("/(txt)|(htm)|(html)|(csv)|(xls)|(doc){1}$/", $file)) echo "$file\n";
+		$file = fopen('import/'.$file.'', "r");
+		while (!feof($file)) {
+		$line_of_text = fgets($file);
+		$input .= addslashes($line_of_text)." ";
+}
+fclose($file);
+    }
 }
 
-fclose($file);
+closedir($handle);
 
 // export to csv file
 csv_to_export($input);
